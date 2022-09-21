4ccb880c42e06f04575d5b577b312328b29869da
AlamHasabie <alamhasabie@yahoo.com>
2022-09-21 16:12:51 -0500

    feat: added junk for test

--- NPCStatusAPISensor.cs
+++ NPCStatusAPISensor.cs
@@ -23,6 +23,10 @@ namespace Simulator.Sensors
     [SensorType("NPCStatus", new System.Type[] { typeof(NPCStatusAPIData) })]
     public class NPCStatusAPISensor : SensorBase
     {
+
+        private BridgeInstance Bridge;
+        private IAgentController Controller;
+
         [SensorParameter]
         public float EventRate = 3f;
         private float EventTimer = 0f;
@@ -62,52 +66,53 @@ namespace Simulator.Sensors
                 if (api != null)
                 {
                     var jsonData = new JSONObject();
-                    foreach(KeyValuePair<string, GameObject> dictObject in api.Agents) 
-                    {
-                        var result = new JSONObject();
-                        var obj = dictObject.Value;
-                        var ped  = obj.GetComponent<PedestrianController>();
+                    jsonData.Add("Junk", "OK !");
+                    // foreach(KeyValuePair<string, GameObject> dictObject in api.Agents) 
+                    // {
+                    //     var result = new JSONObject();
+                    //     var obj = dictObject.Value;
+                    //     var ped  = obj.GetComponent<PedestrianController>();
 
-                        // If it's not a pedestrian
-                        if (ped == null)
-                        {
-                            var tr = obj.transform;
-                            var rb = obj.GetComponent<Rigidbody>();
+                    //     // If it's not a pedestrian
+                    //     if (ped == null)
+                    //     {
+                    //         var tr = obj.transform;
+                    //         var rb = obj.GetComponent<Rigidbody>();
 
-                            var transform = new JSONObject();
-                            transform.Add("position", tr.position);
-                            transform.Add("rotation", tr.rotation.eulerAngles);
+                    //         var transform = new JSONObject();
+                    //         transform.Add("position", tr.position);
+                    //         transform.Add("rotation", tr.rotation.eulerAngles);
 
-                            result.Add("transform", transform);
-                            var npc = obj.GetComponent<NPCController>();
-                            if (npc != null)
-                            {
-                                result.Add("velocity", npc.GetVelocity());
-                                result.Add("angular_velocity", npc.GetAngularVelocity());
-                            }
-                            else
-                            {
-                            result.Add("velocity", rb.velocity);
-                            result.Add("angular_velocity", rb.angularVelocity);
-                            }
-                        }
-                        else 
-                        {
-                            var agent = ped.GetComponent<NavMeshAgent>();
-                            var tr = agent.transform;
+                    //         result.Add("transform", transform);
+                    //         var npc = obj.GetComponent<NPCController>();
+                    //         if (npc != null)
+                    //         {
+                    //             result.Add("velocity", npc.GetVelocity());
+                    //             result.Add("angular_velocity", npc.GetAngularVelocity());
+                    //         }
+                    //         else
+                    //         {
+                    //         result.Add("velocity", rb.velocity);
+                    //         result.Add("angular_velocity", rb.angularVelocity);
+                    //         }
+                    //     }
+                    //     else 
+                    //     {
+                    //         var agent = ped.GetComponent<NavMeshAgent>();
+                    //         var tr = agent.transform;
 
-                            var transform = new JSONObject();
-                            transform.Add("position", tr.position);
-                            transform.Add("rotation", tr.rotation.eulerAngles);
+                    //         var transform = new JSONObject();
+                    //         transform.Add("position", tr.position);
+                    //         transform.Add("rotation", tr.rotation.eulerAngles);
 
-                            result.Add("transform", transform);
-                            result.Add("velocity", agent.velocity);
-                            result.Add("angular_velocity", Vector3.zero);
-                        }
+                    //         result.Add("transform", transform);
+                    //         result.Add("velocity", agent.velocity);
+                    //         result.Add("angular_velocity", Vector3.zero);
+                    //     }
 
-                        // We use UID as the key
-                        jsonData.Add(dictObject.Key, result);
-                    }
+                    //     // We use UID as the key
+                    //     jsonData.Add(dictObject.Key, result);
+                    // }
 
                     api.AddCustom(transform.parent.gameObject, "npcstatus", jsonData);
                 }
